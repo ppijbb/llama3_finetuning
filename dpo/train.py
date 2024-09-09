be0eac16e1bddf20e9d1e60512ae06ca77b99dba
ppijbb <ppijbb@naver.com>
2024-09-09 14:37:41 +0900

    refactor: Update deepspeed_config.yaml for debug mode and mixed precision
    feat: Update train.py for model configuration and Lora settings

--- dpo/train.py
+++ dpo/train.py
@@ -20,8 +20,8 @@ os.environ["WANDB_WATCH"]="0"
 
 # 모델과 토크나이저 불러오기
 model_id = "unsloth/zephyr-sft"
-model_id = "microsoft/Phi-3.5-mini-instruct"
-
+# model_id = "microsoft/Phi-3.5-mini-instruct"
+max_seq_len = 1024
 # model = AutoModelForCausalLM.from_pretrained(model_id,
 #                                              torch_dtype=torch.bfloat16,
 #                                              trust_remote_code=True,
@@ -33,10 +33,13 @@ model_id = "microsoft/Phi-3.5-mini-instruct"
 
 model, tokenizer = FastLanguageModel.from_pretrained(
     model_name = model_id,
-    max_seq_length = 1024,
-    dtype = torch.bfloat16, # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
-    load_in_4bit = True, # Use 4bit quantization to reduce memory usage. Can be False.
+    trust_remote_code=True,
+    use_cache=True, # Use cache for faster decoding
+    max_seq_length=max_seq_len,
+    dtype=torch.bfloat16, # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+    load_in_4bit=False, # Use 4bit quantization to reduce memory usage. Can be False.
     # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+    # attn_implementation="flash_attention_2", 
 )
 
 lora_targets=[
@@ -50,25 +53,25 @@ lora_targets=[
         "o_proj"
     ]
 
-# Lora 설정 정의
-peft_config = LoraConfig(
-    target_modules=lora_targets,
-    r=8,
-    lora_alpha=16,
-    lora_dropout=0.05,
-    bias="none",
-    init_lora_weights="gaussian",
-    task_type="CAUSAL_LM")
-
 # 참조 모델 불러오기 (필요에 따라 수정)
 # ref_model = model
 
 # Lora를 기본 모델에 적용
-# model = get_peft_model(model, peft_config)
+# model = get_peft_model(
+    # model, 
+    # LoraConfig( # Lora 설정 정의
+    # target_modules=lora_targets,
+    # r=8,
+    # lora_alpha=16,
+    # lora_dropout=0.0,
+    # bias="none",
+    # init_lora_weights="gaussian",
+    # task_type="CAUSAL_LM"))
 
 model = FastLanguageModel.get_peft_model(
-    model,
+    model=model,
     r=16,
+    max_seq_length=max_seq_len,
     target_modules=lora_targets,
     lora_alpha=16,
     lora_dropout=0, # Dropout = 0 is currently optimized
@@ -77,15 +80,7 @@ model = FastLanguageModel.get_peft_model(
     random_state=3407,
 )
 # peft_config = LoHaConfig(
-#     target_modules=[
-#         "k_proj",
-#         # "down_proj",
-#         # "gate_proj",
-#         "q_proj",
-#         "v_proj",
-#         # "up_proj",
-#         "o_proj"
-#     ],
+#     target_modules=lora_targets,
 #     r=8,
 #     alpha=32,
 #     task_type="CAUSAL_LM"
@@ -97,12 +92,14 @@ dpo_config = DPOConfig(
     beta=0.1,
     loss_type="ipo",
     max_prompt_length=256,
-    max_target_length=1024,
-    max_length=1024,
+    max_target_length=max_seq_len,
+    max_length=max_seq_len,
+    fp16=False,
     bf16=True,
-    bf16_full_eval=True,
+    # fp16_full_eval=True,
+    # bf16_full_eval=False,
     output_dir="dpo_output",
-    # deepspeed="deepspeed_config.json",
+    optim="adamw_hf", # paged_adamw_8bit adamw_bnb_8bit adamw_8bit
     logging_steps=50,
     num_train_epochs=50,
     gradient_accumulation_steps=64,
@@ -112,10 +109,10 @@ dpo_config = DPOConfig(
 )
 
 # BitsandBytes Paged AdamW Optimizer 설정
-optimizer_class = bnb.optim.PagedAdamW
-optimizer_kwargs = {
-    "lr": dpo_config.learning_rate
-    }
+# optimizer_class = bnb.optim.PagedAdamW
+# optimizer_kwargs = {
+#     "lr": dpo_config.learning_rate
+#     }
 
 dataset = get_dataset(
     dataset_name="Gunulhona/open_dpo_merged",
@@ -137,4 +134,5 @@ dpo_trainer = DPOTrainer(
 
 # DPO를 사용하여 모델 학습
 if __name__ == "__main__":
-    dpo_trainer.train()
+    with torch.autocast("cuda"): 
+        dpo_trainer.train()
