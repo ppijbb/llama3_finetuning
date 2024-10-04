6ed499fdcdddbe5667e63dd9fb1312fb2b698b1b
ppijbb <ppijbb@naver.com>
2024-10-04 17:23:25 +0900

    refactor: Refactor data loader and add test model initialization

--- distillation/student.py
+++ distillation/student.py
@@ -21,3 +21,25 @@ class StudentModel(nn.Module):
         teacher_probs = F.softmax(teacher_output / temperature, dim=1)
         loss = F.kl_div(student_log_probs, teacher_probs, reduction='batchmean') * (temperature ** 2)
         return loss
+    
+from model.moe_model import PhiMoEForCausalLM
+from model.moe_config import PhiMoEConfig
+
+
+test_model = PhiMoEForCausalLM(
+    config=PhiMoEConfig(
+        **{"hidden_size": 4096,
+        "initializer_range": 0.02,
+        "input_jitter_noise": 0.01,
+        "intermediate_size": 6400,
+        "max_position_embeddings": 131072,
+        "model_type": "phimoe",
+        "num_attention_heads": 32,
+        "num_experts_per_tok": 2,
+        "num_hidden_layers": 8,
+        "num_key_value_heads": 8,
+        "num_local_experts": 16,
+        "original_max_position_embeddings": 4096,
+        }
+    ))
+print(test_model.num_parameters())
